bffaa56e2b150f5039b93ab59688d806d4ac2fbb
Gabrielle Arruda <gabriellearrudac@gmail.com>
2022-05-10 11:55:13 -0300

    include buld folder

---
+++ catalog/build/static/2.1f2ffc31.chunk.js
@@ -0,0 +1,27 @@
+webpackJsonp([2], {
+  970: function(n, r, e) {
+    var t = e(5),
+      s = e(48),
+      o = e(370).PageRenderer;
+    o.__esModule && (o = o.default);
+    var i = s({
+      displayName: "WrappedPageRenderer",
+      getInitialState: function() {
+        return { content: e(979) };
+      },
+      componentWillMount: function() {},
+      render: function() {
+        return t.createElement(
+          o,
+          Object.assign({}, this.props, { content: this.state.content })
+        );
+      }
+    });
+    (i.__catalog_loader__ = !0), (n.exports = i);
+  },
+  979: function(n, r) {
+    n.exports =
+      '# Impulso\r\n\r\n```image\r\nplain: true\r\nspan: 6\r\nsrc: "https://raw.githubusercontent.com/ImpulsoGov/design-system/main/estatico/Color%3DBlack%2C%20Size%3DMedium.png"\r\n```\r\n\r\n```download|span-2\r\n{\r\n    "title": "Impulso Previne (.png)",\r\n    "url": "https://raw.githubusercontent.com/ImpulsoGov/design-system/main/estatico/Color%3DBlack%2C%20Size%3DMedium.png"\r\n}\r\n```\r\n\r\n```download|span-2\r\n{\r\n    "title": "Impulso Previne (.svg)",\r\n    "url": "https://raw.githubusercontent.com/ImpulsoGov/design-system/main/estatico/Color%3DBlack%2C%20Size%3DMedium.svg"\r\n}\r\n```\r\n\r\n```download|span-2\r\n{\r\n    "title": "Impulso Previne (.jpg)",\r\n    "url": "https://raw.githubusercontent.com/ImpulsoGov/design-system/main/estatico/Color%3DBlack%2C%20Size%3DMedium.jpg"\r\n}\r\n```\r\n\r\n```image\r\nplain: true\r\nspan: 6\r\nsrc: "https://raw.githubusercontent.com/ImpulsoGov/design-system/main/estatico/Previne.png"\r\n```\r\n\r\n```image\r\nplain: true\r\nspan: 6\r\nsrc: "https://raw.githubusercontent.com/ImpulsoGov/design-system/main/estatico/Previne.png"\r\n```\r\n\r\n```image\r\nplain: true\r\nspan: 6\r\nsrc: "https://raw.githubusercontent.com/ImpulsoGov/design-system/main/estatico/Previne.png"\r\n```\r\n\r\n```image\r\nplain: true\r\nspan: 6\r\nsrc: "https://raw.githubusercontent.com/ImpulsoGov/design-system/main/estatico/Previne.png"\r\n```\r\n\r\n```image\r\nplain: true\r\nspan: 6\r\nsrc: "https://raw.githubusercontent.com/ImpulsoGov/design-system/main/estatico/Previne.png"\r\n```\r\n\r\n# Produtos\r\n\r\n### Impulso Previne\r\n\r\n```image\r\nplain: true\r\nspan: 6\r\nsrc: "https://raw.githubusercontent.com/ImpulsoGov/design-system/main/estatico/Previne.png"\r\n```\r\n\r\n```download|span-2\r\n{\r\n    "title": "Impulso Previne (.png)",\r\n    "url": "https://raw.githubusercontent.com/ImpulsoGov/design-system/main/estatico/Previne.png"\r\n}\r\n```\r\n\r\n```download|span-2\r\n{\r\n    "title": "Impulso Previne (.svg)",\r\n    "url": "https://raw.githubusercontent.com/ImpulsoGov/design-system/main/estatico/Previne.svg"\r\n}\r\n```\r\n\r\n```download|span-2\r\n{\r\n    "title": "Impulso Previne (.jpg)",\r\n    "url": "https://raw.githubusercontent.com/ImpulsoGov/design-system/main/estatico/Previne.jpg"\r\n}\r\n```\r\n';
+  }
+});
+//# sourceMappingURL=2.1f2ffc31.chunk.js.map
