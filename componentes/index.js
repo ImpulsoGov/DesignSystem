88016412ed605081ac08c8bcacb93b774cf311fd
Gabrielle Arruda <gabriellearrudac@gmail.com>
2022-05-18 00:26:47 -0300

    update main file

--- componentes/index.js
+++ componentes/index.js
@@ -1,3 +1,2 @@
-export { default as Dropdown } from "./Dropdown";
-export { default as Tags } from "./Tags";
-export { default as ValuePicker } from "./ValuePicker";
+export { default as Footer } from "./Footer";
+export { default as NavBar } from "./NavBar";
