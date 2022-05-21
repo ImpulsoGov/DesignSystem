8a963653c94bdd5a46fa49ff3d5a8851fbc0c622
Gabrielle Arruda <gabriellearrudac@gmail.com>
2022-05-21 01:14:55 -0300

    index.js attempt fix

---
+++ index.js
@@ -0,0 +1,4 @@
+export { Footer } from "../componentes/Footer/Footer.jsx";
+export { NavBar } from "../componentes/NavBar/NavBar.jsx";
+export { Parcerias } from "../componentes/Parcerias/Parcerias.jsx";
+export { ImageFull } from "../componentes/ImageFull/ImageFull.jsx";
