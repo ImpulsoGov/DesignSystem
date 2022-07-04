16a7877c43e563cfd167c9342dc7c64e3dea0545
dlopes14 <donylopes@gmail.com>
2022-07-04 13:01:48 -0300

    add innerHTML e sanitizer

--- templates/TituloTexto/TituloTexto.jsx
+++ templates/TituloTexto/TituloTexto.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import DOMPurify from "dompurify";
 import "./TituloTexto.css"
 import { ImagensFull2 } from "../../componentes/Imagens/ImagensFull"
+
+const sanitize = (texto) => ({
+  __html: DOMPurify.sanitize(texto)
+})
 
 const ImageTop = ({
   top,
@@ -11,20 +16,29 @@ const ImageTop = ({
     return(
       <div>
         <ImagensFull2 imagem={link} />
-        <div className="tituloTexto">{titulo}</div>
+        <div 
+          className="tituloTexto"
+          dangerouslySetInnerHTML={sanitize(titulo)}
+          ></div>
       </div>
     )
   }else if (top==false) {
     return(
       <div>
-        <div className="tituloTexto">{titulo}</div>
+        <div 
+          className="tituloTexto"
+          dangerouslySetInnerHTML={sanitize(titulo)}
+          ></div>
         <ImagensFull2 imagem={link} />
       </div>
     )
   }else{
     return(
       <div>
-        <div className="tituloTexto">{titulo}</div>
+        <div 
+          className="tituloTexto"
+          dangerouslySetInnerHTML={sanitize(titulo)}
+          ></div>
       </div>
     )
   }
@@ -42,7 +56,10 @@ const TituloTexto = ({
          link = {imagem.url}
          titulo = {titulo}
          />
-        <div className="corpoTexto">{texto}</div>
+        <div 
+          className="corpoTexto"
+          dangerouslySetInnerHTML={sanitize(texto)}
+          ></div>
       </div>
 )};
 
